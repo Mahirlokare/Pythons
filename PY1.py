f9f7811071f28df66161ddf96a14b68f3d692698
Mahir Abdul Majeed Lokare <mahirlokare@gmail.com>
2025-08-14 00:04:32 +0530

    Update PY1.py

--- PY1.py
+++ PY1.py
@@ -1,8 +1,9 @@
-print(" Mahir Lokare")
+print("Your Name")
 def future_value(principal, rate, years):
         	return principal * (1 + rate / 100) ** years
 p = float(input("Enter principal amount: "))
 r = float(input("Enter annual interest rate (in %): "))
 n = int(input("Enter number of years: "))
 print("Future Value:", round(future_value(p, r, n), 2))
+
 print("---Execution is Done---")
